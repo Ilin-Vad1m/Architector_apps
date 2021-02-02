aa16b682759a2938f5fe9ab5141cee4414aa46d8
Vadim <vad2.com@gmail.com>
2021-02-02 20:00:58 +0300

    ain_factory.java

---
+++ main_factory.java
@@ -0,0 +1,12 @@
+public class App{
+	public static void main(String[] args) {
+		ElementsFactory elementsFactory;
+		private auth boolean = true;
+		if(auth)
+			elementsFactory = AuthenticationWindow();
+		else
+			elementsFactory = MainWindow();
+		
+	}
+
+}
